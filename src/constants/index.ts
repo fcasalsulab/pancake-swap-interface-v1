172b93e441021b498a737615c8cf0b06d03d2081
Halaprix <panicz.klos@gmail.com>
2022-01-19 14:43:28 +0100

    new router

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 import { ChainId, JSBI, Percent, Token, WETH } from '@pancakeswap-libs/sdk'
 
-export const ROUTER_ADDRESS = '0x250d48C5E78f1E85F7AB07FEC61E93ba703aE668'
+export const ROUTER_ADDRESS = '0xe8611f3818aa7d95e7e62f598d2627f137c1b6c7'
 
 // a list of tokens by chain
 type ChainTokenList = {
